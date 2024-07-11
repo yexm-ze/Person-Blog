bedb0f7ed9ee6c6a001fe9fe7618c9c89d888061
yexuemei <yexuemei@didapinche.com>
2024-07-12 03:13:43 +0800

    add baseurl for assent

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,31 +1,31 @@
 const swconf = {
-  
-    cacheName: 'chirpy-1720722011',resources: [
-      '/assets/css/jekyll-theme-chirpy.css',
-      '/',
-      
-        '/categories/',
-      
-        '/tags/',
-      
-        '/archives/',
-      
-        '/about/',
-      
 
-      
-      
-        '/assets/js/dist/app.min.js',
-        '/assets/js/dist/categories.min.js',
-        '/assets/js/dist/commons.min.js',
-        '/assets/js/dist/home.min.js',
-        '/assets/js/dist/misc.min.js',
-        '/assets/js/dist/page.min.js',
-        '/assets/js/dist/post.min.js'
+    cacheName: 'chirpy-1720722011',resources: [
+      '/Personal-Blog/assets/css/jekyll-theme-chirpy.css',
+      '/',
+
+        '/categories/',
+
+        '/tags/',
+
+        '/archives/',
+
+        '/about/',
+
+
+
+
+        '/Personal-Blog/assets/js/dist/app.min.js',
+        '/Personal-Blog/assets/js/dist/categories.min.js',
+        '/Personal-Blog/assets/js/dist/commons.min.js',
+        '/Personal-Blog/assets/js/dist/home.min.js',
+        '/Personal-Blog/assets/js/dist/misc.min.js',
+        '/Personal-Blog/assets/js/dist/page.min.js',
+        '/Personal-Blog/assets/js/dist/post.min.js'
     ],denyPaths: [
-      
+
     ],
     purge: false
-  
+
 };
 
